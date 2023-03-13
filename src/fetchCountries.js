c4b35115da9ece6e8f87e22c628c86bf7d78509a
Pasichnyk Vasyl <pasichnik-v@ukr.net>
2023-03-13 20:32:51 +0200

    base

---
+++ src/fetchCountries.js
@@ -0,0 +1,6 @@
+export { fetchCountries };
+
+
+  function fetchCountries(name) {
+return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,flags,capital,population,languages`)
+  .then(response => response.json())
